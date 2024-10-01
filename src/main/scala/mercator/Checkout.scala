6983882590f18f0c77ad0097f9f6f47d4d2f32d7
Dario Abdulrehman <dabdulrehman@equalexperts.com>
2024-10-01 10:09:44 +0100

    Step 2: checkout with offers

--- src/main/scala/mercator/Checkout.scala
+++ src/main/scala/mercator/Checkout.scala
@@ -5,4 +5,12 @@ object Checkout {
   def calculateTotal(items: List[Item]): BigDecimal =
     items.map(_.price).sum
 
+  def calculateTotalWithOffers(items: List[Item]): BigDecimal = {
+    val itemCounts = items.groupBy(identity).map { case (k, v) => k -> v.size }
+    val appleCount = itemCounts.getOrElse(Apple, 0)
+    val orangeCount = itemCounts.getOrElse(Orange, 0)
+    val appleTotal = (appleCount / 2 + appleCount % 2) * Apple.price
+    val orangeTotal = ((orangeCount / 3) * 2 + orangeCount % 3) * Orange.price
+    appleTotal + orangeTotal
+  }
 }
--- src/test/scala/mercator/CheckoutSpec.scala
+++ src/test/scala/mercator/CheckoutSpec.scala
@@ -7,4 +7,24 @@ class CheckoutSpec extends FunSuite {
     val cart = List(Apple, Apple, Orange, Apple)
     assertEquals(Checkout.calculateTotal(cart), BigDecimal("2.05"))
   }
+
+  test("calculateTotalWithOffers [Apple, Apple]") {
+    val cart = List(Apple, Apple)
+    assertEquals(Checkout.calculateTotalWithOffers(cart), BigDecimal("0.60"))
+  }
+
+  test("calculateTotalWithOffers [Orange, Orange, Orange]") {
+    val cart = List(Orange, Orange, Orange)
+    assertEquals(Checkout.calculateTotalWithOffers(cart), BigDecimal("0.50"))
+  }
+
+  test("calculateTotalWithOffers [Apple, Apple, Apple, Orange, Orange, Orange, Orange]") {
+    val cart = List(Apple, Apple, Apple, Orange, Orange, Orange, Orange)
+    assertEquals(Checkout.calculateTotalWithOffers(cart),
+      BigDecimal("0.60") + // 2-for-1 apple
+      BigDecimal("0.60") + //1 apple
+        BigDecimal("0.50") + // 3-for-2 oranges
+        BigDecimal("0.25") // 1 orange
+    )
+  }
 }
